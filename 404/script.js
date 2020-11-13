b8c26af73cf1ebf70d8a28c4133f2599cbca6fe9
[Sagyam] <[sagyamthapa32@gmail.com]>
2020-11-13 11:52:07 +0545

    completed

---
+++ 404/script.js
@@ -0,0 +1,7 @@
+var container = document.getElementById("container");
+window.onmousemove = function (e) {
+  var x = -e.clientX / 5;
+  var y = -e.clientY / 5;
+  container.style.backgroundPositionX = x + "px";
+  container.style.backgroundPositionY = y + "px";
+};
